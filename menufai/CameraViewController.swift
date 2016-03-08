b79b4a37ccaaa13ed258704150534a4acb348bbf
varun vyas <moduvyas@gmail.com>
2016-03-08 18:39:29 -0500

    Merge remote-tracking branch 'origin/bareBones' into VarunBranch
    # Conflicts:
    #	.DS_Store
    #	menufai.xcodeproj/project.pbxproj
    #	menufai/Base.lproj/LaunchScreen.storyboard
    #	menufai/Base.lproj/Main.storyboard
    #	menufai/MenufaiViewController.swift

---
+++ menufai/CameraViewController.swift
@@ -0,0 +1,48 @@
+//
+//  CameraViewController.swift
+//  menufai
+//
+//  Created by Chun Kwok on 2/23/16.
+//  Copyright © 2016 Varun Vyas. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        
+        self.presentViewController(vc, animated: true, completion: nil)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func imagePickerController(picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+            let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+            let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
